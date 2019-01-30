1e023fc1b6aed1bdd97ab721cb8f52fdd420a436
jpcy1998 <jpcy18@gmail.com>
2019-01-30 14:03:34 -0500

    Un mono

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isActive = true;
+  isActive = false;
   title = 'Angular ';
   imagenurl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgyHnnKUoPvbeg00psQ3rnVjeynE6F5lmfUVxh0P2uoc0aEyYd";
 }
